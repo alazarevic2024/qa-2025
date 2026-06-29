00d1f9122a11a8ad530c69cdff28c6787fad18ce
Aleksandra <aleksandra.lazarevic@it-akademija.com>
2026-06-29 20:42:55 +0200

    niti - threading

---
+++ 2026_06_29/kasa.py
@@ -0,0 +1,30 @@
+import time
+import threading
+
+stanje_u_kasi = 0
+
+kasa_lock = threading.Lock() # kljuc
+
+def dodaj_u_kasu(iznos):
+    global stanje_u_kasi
+
+    #kasa_lock.acquire() zakljucavam
+    with kasa_lock:
+        # citanje trenutnog stanja
+        trenutno_stanje = stanje_u_kasi
+        time.sleep(0.001)
+        # upis novog stanja
+        stanje_u_kasi = trenutno_stanje + iznos
+    #kasa_lock.release() otkljucavam
+
+niti = []
+for i in range(100):
+    nit = threading.Thread(target=dodaj_u_kasu, args=(10,))
+    niti.append(nit)
+    nit.start()
+
+for nit in niti:
+    nit.join()
+
+print("Ocekivano stanje: 1000")
+print(f"Realno stanje: {stanje_u_kasi}")
---
+++ 2026_06_29/pr_bez_niti.py
@@ -0,0 +1,26 @@
+import time
+import threading
+
+def ucitaj_podatke(naziv):
+    print(f"Pocinjem ucitavanje: {naziv}")
+    time.sleep(3)
+    print(f"Zavrseno ucitavanje {naziv}")
+
+print("Aplikacija se pokrece")
+# ucitaj_podatke("korisnik")
+# ucitaj_podatke("proizvodi")
+# ucitaj_podatke("notifikacije")
+
+nit1 = threading.Thread(target=ucitaj_podatke, args=("korisnik",))
+nit2 = threading.Thread(target=ucitaj_podatke, args=("proizvodi",))
+nit3 = threading.Thread(target=ucitaj_podatke, args=("notifikacije",))
+
+nit1.start()
+nit2.start()
+nit3.start()
+
+nit1.join()
+nit2.join()
+nit3.join()
+
+print("Zavrseno izvrsavanje programa")
